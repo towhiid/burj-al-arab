9c5b3be902b232dddc64e48696643e75409b4115
towhiid <towhid3008@gmail.com>
2020-09-17 00:18:52 +0600

    burj-al dubai

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBtNLwUIRK7W8iW-GxUhALSyGgYFWG8Rk8",
+    authDomain: "burj-al-arab-89c75.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-89c75.firebaseio.com",
+    projectId: "burj-al-arab-89c75",
+    storageBucket: "burj-al-arab-89c75.appspot.com",
+    messagingSenderId: "957360524103",
+    appId: "1:957360524103:web:88e01996cb8883c7e09a62"
+  };
+  export default firebaseConfig;
